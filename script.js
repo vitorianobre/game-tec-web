dfc4cc2b113647524aa12afb13189580aa3ecc34
vitoria <vitorianobre@alu.ufc.br>
2023-07-09 16:24:40 -0300

    novas alteracoes

--- script.js
+++ script.js
@@ -9,61 +9,68 @@ function criaPista() {
     this.elemento.src = '/personagens/pista.png'
 }
 
-function NovoCarro(largura, altura) {
+function NovoCarro(largura, altura, popsicaoNaTela) {
     this.elemento = novoElemento('img', 'par-de-barreiras')
     this.elemento.src = '/personagens/luigi.png'
 
-    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
-    this.setX = (posicaoNaTela) => this.elemento.style.left = `${posicaoNaTela}px`
-    this.getY = () => parseInt(this.elemento.style.top.split('px')[0])
-    this.setY = (posicaoNaTela) => this.elemento.style.top = `${posicaoNaTela}px`
-    this.getAltura = () => this.elemento.clientHeight
-
-    // let posicaoXAleatoria = Math.floor(Math.random() * (400 - this.getAltura()))
-    let posicaoXAleatoria = Math.floor(Math.random() * 380)
-    let posicaoYAleatoria = Math.floor(Math.random() * 300)
-    // let posicaoYAleatoria = Math.floor(Math.random() * (altura - this.getAltura()))
-
-    // let margemX = (800 - this.getAltura()) / 2 // Margem horizontal para centralizar
-    let margemX = ((largura / 2) - 200) // Margem horizontal para centralizar
-    let margemY = (altura - this.getAltura()) / 2 // Margem vertical para centralizar
-
-    this.setX(posicaoXAleatoria + margemX)
-    this.setY(posicaoYAleatoria )
-}
-
-function Corredores(largura, altura) {
-    this.jogadores = []
-
-    for (let i = 0; i < 3; i++) {
-        const jogador = new NovoCarro(largura, altura);
-        this.jogadores.push(jogador)
+    this.sortearPosicao = () => {
+        let posicaoXAleatoria = Math.floor(Math.random() * 350)
+        let posicaoYAleatoria = Math.floor(Math.random() * altura)
+        let margemX = ((largura / 2) - 190) // Margem horizontal para centralizar
+        let margemY = (altura - this.getAltura()) / 2 // Margem vertical para centralizar
+        this.setX(posicaoXAleatoria + margemX)
+        this.setY(posicaoYAleatoria + margemY)
     }
 
-    const deslocamento = 3;
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = (posicaoNaTela) => this.elemento.style.left = `${posicaoNaTela}px`
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = (posicaoNaTela) => this.elemento.style.bottom = `${posicaoNaTela}px`
+    this.getAltura = () => this.elemento.clientHeight
+
+    this.sortearPosicao()
+    this.setY(popsicaoNaTela)
+} 
+
+function Corredores(largura, altura, espaco, notificarPonto) {
+    this.jogadores = [
+        new NovoCarro(largura, altura, altura),
+        new NovoCarro(largura, altura, altura),
+        new NovoCarro(largura, altura, altura)
+    ]
+
+    const deslocamento = 2
     this.animar = () => {
         this.jogadores.forEach(jogador => {
-            const yAtual = jogador.getY()
-            const novaPosicao = yAtual + deslocamento
+            jogador.setY(jogador.getY() - deslocamento)
 
-            if (novaPosicao > altura) {
-                jogador.setY(-jogador.getAltura())           
-            } else {
-                jogador.setY(novaPosicao)
+            if (jogador.getY() < -jogador.getAltura()) {
+                jogador.setY(jogador.getY() + espaco * this.jogadores.length)
+                jogador.sortearPosicao()
             }
-        });
-    };
+        })
+    }
 }
 
 function Car(larguraJogo) {
     let movingLeft = false
     let movingRight = false
+    let deslocamento = 8
 
     this.elemento = novoElemento('img', 'car')
     this.elemento.src = '/personagens/mario.png'
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
     this.setX = y => this.elemento.style.left = `${y}px`
+
+    this.saiuDaPista = () => {
+        let posicaoX = this.getX()
+        if (posicaoX < 450 || posicaoX > 800) {
+            deslocamento = 2
+        } else {
+            deslocamento = 8
+        }
+    }
 
     window.onkeydown = e => {
         if (e.which == '37') {
@@ -83,10 +90,13 @@ function Car(larguraJogo) {
 
         let novoX = this.getX();
 
+        
         if (movingRight) {
-            novoX += 15
+            this.saiuDaPista()
+            novoX += deslocamento
         } else if (movingLeft) {
-            novoX -= 15
+            this.saiuDaPista()
+            novoX -= deslocamento
         }
 
         if (novoX <= 0) {
@@ -100,8 +110,29 @@ function Car(larguraJogo) {
     this.setX(larguraJogo / 2)
 }
 
-function Progresso() {
+function estaoSobrepostos(elementoA, elementoB) {
 
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function colidiu(carroMario, corredores) {
+    let colidiu = false
+
+    corredores.jogadores.forEach(jogador => {
+        if (!colidiu) {
+            colidiu = estaoSobrepostos(carroMario.elemento, jogador.elemento)
+        }
+    })
+    return colidiu
+
+}
+
+function Progresso() {
     this.elemento = novoElemento('span', 'progresso')
     this.atualizarPontos = pontos => {
         this.elemento.innerHTML = pontos
@@ -115,35 +146,28 @@ function Game() {
     const altura = areaDoJogo.clientHeight
     const largura = areaDoJogo.clientWidth
 
-    // const progresso = new Progresso()
-    const corredores = new Corredores(largura, altura)
+    const progresso = new Progresso()
+    const corredores = new Corredores(largura, altura, 300, () => progresso.atualizarPontos(++pontos))
 
     const car = new Car(largura)
 
     const pista = new criaPista()
 
-    // areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(progresso.elemento)
     areaDoJogo.appendChild(car.elemento)
     areaDoJogo.appendChild(pista.elemento)
 
     corredores.jogadores.forEach(jogador => areaDoJogo.appendChild(jogador.elemento))
 
-    // this.start = () => {
-    //     const temporizador = setInterval(() => {
-    //         corredores.animar()
-    //         passaro.animar()
-
-    //           if(colidiu(passaro,corredores)){
-    //              clearInterval(temporizador) 
-    //          } 
-    //     }, 20)
-    // }
-
     this.start = () => {
         const temporizador = setInterval(() => {
             corredores.animar()
             car.animar()
-        }, 20)
+
+            if(colidiu(car, corredores)){
+                clearInterval(temporizador)
+            } 
+        }, 10)
     }
 }
 
