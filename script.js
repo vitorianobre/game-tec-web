b428de9bc45ee8d668339bfaa571bfbcd2cb1e80
vitoria <vitorianobre@alu.ufc.br>
2023-07-09 01:25:20 -0300

    modificacoes

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function novoElemento(tagName, className) {
     return elemento
 }
 
-function criaPista(quantidadePistas) {
-    this.elemento = novoElemento('img', 'barreiras')
+function criaPista() {
+    this.elemento = novoElemento('img', 'pista')
     this.elemento.src = '/personagens/pista.png'
 }
 
@@ -19,34 +19,35 @@ function NovoCarro(largura, altura) {
     this.setY = (posicaoNaTela) => this.elemento.style.top = `${posicaoNaTela}px`
     this.getAltura = () => this.elemento.clientHeight
 
-    var posicaoXAleatoria = Math.floor(Math.random() * (400 - this.getAltura()))
-    var posicaoYAleatoria = Math.floor(Math.random() * (altura - this.getAltura()))
+    // let posicaoXAleatoria = Math.floor(Math.random() * (400 - this.getAltura()))
+    let posicaoXAleatoria = Math.floor(Math.random() * 380)
+    let posicaoYAleatoria = Math.floor(Math.random() * 300)
+    // let posicaoYAleatoria = Math.floor(Math.random() * (altura - this.getAltura()))
 
-    var margemX = (800 - this.getAltura()) / 2 // Margem horizontal para centralizar
-    var margemY = (altura - this.getAltura()) / 2 // Margem vertical para centralizar
+    // let margemX = (800 - this.getAltura()) / 2 // Margem horizontal para centralizar
+    let margemX = ((largura / 2) - 200) // Margem horizontal para centralizar
+    let margemY = (altura - this.getAltura()) / 2 // Margem vertical para centralizar
 
     this.setX(posicaoXAleatoria + margemX)
-    this.setY(posicaoYAleatoria + margemY)
+    this.setY(posicaoYAleatoria )
 }
 
-function Corredores(largura, altura, quantidadeJogadores) {
+function Corredores(largura, altura) {
     this.jogadores = []
 
-    var quantidadeMaximaJogadores = Math.min(quantidadeJogadores, 3) // Limita a quantidade de jogadores em até 3
-
-    for (var i = 0; i < quantidadeMaximaJogadores; i++) {
-        var jogador = new NovoCarro(largura, altura);
+    for (let i = 0; i < 3; i++) {
+        const jogador = new NovoCarro(largura, altura);
         this.jogadores.push(jogador)
     }
 
     const deslocamento = 3;
     this.animar = () => {
         this.jogadores.forEach(jogador => {
-            var yAtual = jogador.getY()
-            var novaPosicao = yAtual + deslocamento
+            const yAtual = jogador.getY()
+            const novaPosicao = yAtual + deslocamento
 
             if (novaPosicao > altura) {
-                jogador.setY(-jogador.getAltura())
+                jogador.setY(-jogador.getAltura())           
             } else {
                 jogador.setY(novaPosicao)
             }
@@ -99,6 +100,15 @@ function Car(larguraJogo) {
     this.setX(larguraJogo / 2)
 }
 
+function Progresso() {
+
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
 function Game() {
     let pontos = 0
     const areaDoJogo = document.querySelector('[game]')
@@ -106,7 +116,7 @@ function Game() {
     const largura = areaDoJogo.clientWidth
 
     // const progresso = new Progresso()
-    const corredores = new Corredores(largura, altura, 200)
+    const corredores = new Corredores(largura, altura)
 
     const car = new Car(largura)
 
@@ -135,15 +145,6 @@ function Game() {
             car.animar()
         }, 20)
     }
-}
-
-function Progresso() {
-
-    this.elemento = novoElemento('span', 'progresso')
-    this.atualizarPontos = pontos => {
-        this.elemento.innerHTML = pontos
-    }
-    this.atualizarPontos(0)
 }
 
 new Game().start()
